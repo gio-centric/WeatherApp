bfa26e2684135d181c3e2bd2cadcf160a21fc82d
gio-centric <giovanny.maya@hotmail.com>
2024-11-04 22:14:25 -0500

    Update app.js
    Cleanup - will not use weather gifs

--- app.js
+++ app.js
@@ -15,29 +15,6 @@ document.getElementById('weatherForm').addEventListener('submit', function(e) {
                 //Get the appropriate weather icon
                 const iconCode = data.weather[0].icon;
                 const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
-
-                //const weatherCondition = data.weather[0].main.toLowerCase(); // Main weather condition (e.g., Rain, Snow)
-                //let gifUrl = '';
-
-                // You can customize this mapping based on your available GIFs
-                // if (weatherCondition.includes('rain')) {
-                //     gifUrl = './Weather_Gifs/rain.gif';
-                // } else if (weatherCondition.includes('clear')) {
-                //     gifUrl = './Weather_Gifs/sun.gif';
-                // } else if (weatherCondition.includes('snow')) {
-                //     gifUrl = './Weather_Gifs/snow.gif';
-                // } else if (weatherCondition.includes('clouds')) {
-                //     gifUrl = './Weather_Gifs/cloudy.gif';
-                // } else if (weatherCondition.includes('thunderstorm')) {
-                //     gifUrl = './Weather_Gifs/storm.gif';
-                // } else if (weatherCondition.includes('drizzle')) {
-                //     gifUrl = './Weather_Gifs/rain.gif';
-                // } else if (weatherCondition.includes('evening')) {
-                //     gifUrl = './Weather_Gifs/night.gif';
-                // } else {
-                //     gifUrl = './Weather_Gifs/default.gif'; // A default GIF if none match
-                // }
-
                 const weatherResult = `
                     <h2>Weather in ${data.name}</h2>
                     <img src="${iconUrl}" alt="${data.weather[0].description}">
@@ -46,9 +23,6 @@ document.getElementById('weatherForm').addEventListener('submit', function(e) {
                     <p>Humidity: ${data.main.humidity}%</p>
                     <p>Wind Speed: ${data.wind.speed}${wind_speed_label}</p>
                 `;
-
-                //<img src="${gifUrl}" alt="Weather animation" class="weather-gif"> //ADD this above in last string template literal to use gifURL
-                
                 document.getElementById('weatherResult').innerHTML = weatherResult;
             } else {
                 document.getElementById('weatherResult').innerHTML = `<p>City cannot be found!</p>`;
